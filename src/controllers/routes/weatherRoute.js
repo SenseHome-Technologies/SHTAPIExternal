50abc85f300976b433e264895ec436383c9f708c
TinoMeister <tiagooliveira869@gmail.com>
2024-12-07 05:56:04 +0000

    Update swagger

--- src/controllers/routes/weatherRoute.js
+++ src/controllers/routes/weatherRoute.js
@@ -8,29 +8,30 @@ const router = require('express').Router();
  * @api {post} /api/weather/currentWeather Get Current Weather
  * @apiName GetCurrentWeather
  * @apiGroup Weather
- * 
+ * @apiDescription Retrieve current weather information based on latitude and longitude.
+ *
  * @apiParam {Number} [latitude] Latitude of the location.
  * @apiParam {Number} [longitude] Longitude of the location.
  *
  * @apiSuccess {Number} status Response status code.
- * @apiSuccess {Object} token Weather data.
- * 
+ * @apiSuccess {Object} data Weather data.
+ *
  * @apiError {Number} status Error status code.
  * @apiError {String} message Error message.
  */
 router.route('/weather/currentWeather').post(
     // Define an asynchronous function to handle the currentWeather route
     async (req, res) => {
-        // Extract latitude, longitude and exclude from the request body
+        // Extract latitude and longitude from the request body
         const {latitude, longitude} = req.body;
 
         try {
-            // Use weatherInteractorOpenWeather to attempt get the weather with the provided data
+            // Use weatherInteractorOpenWeather to attempt to get the weather with the provided data
             const weather = await weatherInteractorOpenWeather.currentWeather({currentWeatherPersistence}, {latitude, longitude});
             // Send the response with the status and weather data
             res.status(weather.status).send(weather);
         } catch (err) {
-            // Log any errors that occur during the login process
+            // Log any errors that occur during the process
             console.log(err);
             // Rethrow the error to be handled by the caller
             throw err;
